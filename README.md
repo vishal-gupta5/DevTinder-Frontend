8e215803d822a030490e374a234c564428afb360
Vishal Gupta <guptaaryan1103@gmail.com>
2026-02-03 16:20:06 +0530

    Install react-router-dom, Handling the routing and Create a Footer Component

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 #Frontend
+
 - Create a Vite + React Applications
 - Remove all necessary code from Vite Applications
 - Install the tailwindcss and daisyUI depandancies
+- Create a Navbar.js
+- Install the react-router-dom
+- Create BrowserRouter > Routes > Route=/Body > RouteChildren
+- Create an Outlet in Body component.
+- Create a Footer component inside the Body.js
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
